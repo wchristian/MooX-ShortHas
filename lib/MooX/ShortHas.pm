3c9af352925103a3ac20d8395a71e330e1453e0a
Christian Walde <walde.christian@gmail.com>
2017-08-10 23:53:36 +0200

    initial commit

---
+++ lib/MooX/ShortHas.pm
@@ -0,0 +1,11 @@
+package MooX::ShortHas;
+
+use strictures 2;
+
+# VERSION
+
+# ABSTRACT: shortcuts for common Moo has attribute configurations
+
+# COPYRIGHT
+
+1;
---
+++ t/basic.t
@@ -0,0 +1,15 @@
+use strictures 2;
+
+use Test::InDistDir;
+use Test::More;
+
+use MooX::ShortHas;
+
+run();
+done_testing;
+exit;
+
+sub run {
+
+    return;
+}
